992f9e4008ae6281f7df46240c06a74cc01fd57a
Santiago Garcia <santiago.garcia@posteo.de>
2023-11-26 11:09:25 +0100

    A few keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+if not vim.g.vscode then
+  keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+  keymap("n", "<C-t>", ":Telescope lsp_dynamic_workspace_symbols <CR>", opts)
+end
