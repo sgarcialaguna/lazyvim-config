0e308d40259f764b2a6013ef062d57e6af7ad69a
Santiago Garcia <santiago.garcia@posteo.de>
2023-11-27 08:44:29 +0100

    Make surround.mini work more like vim-surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,10 @@ if not vim.g.vscode then
   keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
   keymap("n", "<C-t>", ":Telescope lsp_dynamic_workspace_symbols <CR>", opts)
 end
+
+-- Remap adding surrounding to Visual mode selection
+vim.keymap.del("x", "ys")
+vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+-- Make special mapping for "add surrounding for line"
+vim.keymap.set("n", "yss", "ys_", { remap = true })
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -11,4 +11,24 @@ return {
   { "hiphish/rainbow-delimiters.nvim" },
   -- buffers are scoped to tabs
   { "stevearc/stickybuf.nvim", config = true },
+  -- Make mini.surround work close to vim-surround
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+        update_n_lines = "",
+
+        -- Add this only if you don't want to use extended mappings
+        suffix_last = "",
+        suffix_next = "",
+      },
+      search_method = "cover_or_next",
+    },
+  },
 }
