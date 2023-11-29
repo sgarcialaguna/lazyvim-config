c15c51e95e9b40cc600ccd5c93a8211a24720f37
Santiago Garcia <santiago.garcia@mevis.de>
2023-11-29 09:40:38 +0100

    Python improvements

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,6 +6,7 @@ return {
       cut_key = "x",
       override_del = true,
     },
+    vscode = true,
   },
   -- rainbow brackets
   { "hiphish/rainbow-delimiters.nvim" },
@@ -32,5 +33,12 @@ return {
     },
   },
   -- Git integration
-  { "tpope/vim-fugitive" },
+  { "tpope/vim-fugitive", vscode = true },
+  -- point venv selector to poetry virtualenvs
+  {
+    "linux-cultist/venv-selector.nvim",
+    opts = {
+      poetry_path = os.getenv("LOCALAPPDATA") .. "\\pypoetry\\Cache\\virtualenvs",
+    },
+  },
 }
